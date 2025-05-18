5d29d3651ca8e15b2600cf5c8eeb7b2eeb42289f
Juan <david.2727@gmail.com>
2025-05-17 23:26:17 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import 'dotenv/config';
 import alertasRouter from "./routers/alertasRouter.js"
-
+import cors from 'cors'
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 
