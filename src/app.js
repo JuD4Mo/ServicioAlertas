e4408779b4ae2909b9dfa65fc660866a23ac16f6
Zulu <juanpablozuluaga.0311@gmail.com>
2025-09-10 09:31:57 -0500

    Endpoint health añadido

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/health", (req, res) => res.send("OK"));
 app.use("/alertas", alertasRouter);
 
 const PORT = 3000;
